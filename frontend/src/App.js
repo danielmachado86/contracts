a9c0f224f9ee6c662aa8d4a605cda86b20b434de
Daniel Machado <danielmcis@gmail.com>
2022-04-06 11:12:48 +0000

    Added frontend package

---
+++ frontend/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+
+import './App.css';
+
+import Admin from './Admin.js'
+import CoffeeList from './CoffeeList.js'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Coffee Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/admin">Admin</Nav.Link>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+        <Routes>
+          <Route path="/admin">
+            <Admin/>
+          </Route>
+          <Route path="/">
+            <CoffeeList/>
+          </Route>
+          <Route>
+            <CoffeeList/>
+          </Route>
+        </Routes>
+        </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
