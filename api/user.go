093daf73bc78f18d5cfac7c6541bc01163e7f7e9
Daniel Machado <danielmcis@gmail.com>
2022-07-04 01:44:40 +0000

    Added refresh token compatibility

--- api/user.go
+++ api/user.go
@@ -8,6 +8,7 @@ import (
 	db "github.com/danielmachado86/contracts/db/sqlc"
 	"github.com/danielmachado86/contracts/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/lib/pq"
 )
 
@@ -93,8 +94,12 @@ type loginUserRequest struct {
 }
 
 type loginUserResponse struct {
-	AccessToken string       `json:"access_token"`
-	User        userResponse `json:"user"`
+	SessionId             uuid.UUID    `json:"session_id"`
+	AccessToken           string       `json:"access_token"`
+	AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at"`
+	RefreshToken          string       `json:"refresh_token"`
+	RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
+	User                  userResponse `json:"user"`
 }
 
 func (server *Server) createSessions(ctx *gin.Context) {
@@ -123,7 +128,7 @@ func (server *Server) createSessions(ctx *gin.Context) {
 		return
 	}
 
-	accessToken, err := server.tokenMaker.CreateToken(
+	accessToken, accessTokenPayload, err := server.tokenMaker.CreateToken(
 		user.Username,
 		server.config.AccessTokenDuration,
 	)
@@ -132,9 +137,37 @@ func (server *Server) createSessions(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err, http.StatusInternalServerError))
 	}
 
+	refreshToken, refreshTokenPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.RefreshTokenDuration,
+	)
+	if err != nil {
+		server.Logger.Errorf("failed to create token for user: ", req.Username)
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err, http.StatusInternalServerError))
+	}
+
+	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           refreshTokenPayload.ID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    "",
+		ClientIp:     "",
+		IsBlocked:    false,
+		ExpiresAt:    refreshTokenPayload.ExpiredAt,
+		CreatedAt:    refreshTokenPayload.IssuedAt,
+	})
+	if err != nil {
+		server.Logger.Errorf("failed to create session for user: ", req.Username)
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err, http.StatusInternalServerError))
+	}
+
 	rsp := loginUserResponse{
-		AccessToken: accessToken,
-		User:        newUserResponse(user),
+		SessionId:             session.ID,
+		AccessToken:           accessToken,
+		AccessTokenExpiresAt:  accessTokenPayload.ExpiredAt,
+		RefreshToken:          refreshToken,
+		RefreshTokenExpiresAt: refreshTokenPayload.ExpiredAt,
+		User:                  newUserResponse(user),
 	}
 
 	server.Logger.Infof("user %s succesfully authenticated", req.Username)
