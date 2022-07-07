43afc4b8df76157cc4318b1b6a36b612e723c248
danielmachado86 <danielmcis@gmail.com>
2022-07-07 13:51:26 -0500

    Get project directory from env variables (#36)

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,14 @@
-git clone https://github.com/zsh-users/zsh-autosuggestions /home/vscode/.oh-my-zsh/custom/plugins/zsh-autosuggestions &&
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/vscode/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && 
-git clone https://github.com/romkatv/powerlevel10k.git /home/vscode/.oh-my-zsh/custom/themes/powerlevel10k && 
-cp /workspaces/contracts/.devcontainer/.zshrc /home/vscode/ && 
-cp /workspaces/contracts/.devcontainer/.p10k.zsh /home/vscode/ && 
-git config --global user.email \"danielmcis@gmail.com\" && 
+#!/bin/bash
+
+# clone terminal plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
+
+# load plugins config files
+cp $PWD/.devcontainer/.zshrc $HOME
+cp $PWD/.devcontainer/.p10k.zsh $HOME
+
+# configure git user info
+git config --global user.email \"danielmcis@gmail.com\"
 git config --global user.name \"Daniel Machado\"
