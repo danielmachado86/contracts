02cdc5aca408da2ac4c5481c13f1cd59224c0d59
Daniel Machado <danielmcis@gmail.com>
2022-07-25 15:01:14 +0000

    rules engine refactored

--- rules/rules_test.go
+++ rules/rules_test.go
@@ -1,120 +1,143 @@
 package rules
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"testing"
 	"time"
 
 	"github.com/danielmachado86/contracts/data"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSignatureDateRule(t *testing.T) {
 
-	parties := []*data.Party{
-		data.Party1,
-		data.Party2,
+	file, err := ioutil.ReadFile("rental_contract.json")
+	require.NoError(t, err)
+
+	template := TemplateSpecification{}
+	err = json.Unmarshal([]byte(file), &template)
+	require.NoError(t, err)
+
+	user1 := data.NewUser("Daniel M")
+	user2 := data.NewUser("Jimena L")
+
+	party1 := data.NewParty(user1)
+	party2 := data.NewParty(user2)
+
+	data.NewSignature(party1)
+	data.NewSignature(party2)
+
+	var parties []*data.Party
+	parties = append(parties, party1)
+	parties = append(parties, party2)
+
+	ruleRegistry := make(map[string]Rule)
+
+	for _, attribute := range template.Attributes {
+		ruleRegistry[attribute.Name], err = CreateRule(attribute)
+		require.NoError(t, err)
 	}
 
-	r := SignatureDate{parties: parties}
-	r.Compute()
+	ruleRegistry["contract_parties"].(*Parties).Parties = parties
 
-	got := r.task.GetDate()
+	rule := ruleRegistry["contract_signature_date"]
+	err = rule.Run(ruleRegistry)
+	require.NoError(t, err)
 
-	want := time.Date(2022, 4, 27, 15, 0, 0, 0, time.UTC)
-
-	if got != want {
-		t.Error(fmt.Print("Got: ", got, "\n", "Want: ", want, "\n"))
-	}
-}
-
-func TestStartDateRule(t *testing.T) {
-	parties := []*data.Party{
-		data.Party1,
-		data.Party2,
-	}
-
-	r := StartDate{parties: parties}
-	r.Compute()
-
-	got := r.task.GetDate()
-
-	want := time.Date(2022, 4, 28, 0, 0, 0, 0, time.UTC)
-
-	if got != want {
-		t.Error(fmt.Print("Got: ", got, "\n", "Want: ", want, "\n"))
-	}
-}
-
-func TestEndDateRule(t *testing.T) {
-
-	parties := []*data.Party{
-		data.Party1,
-		data.Party2,
-	}
-
-	r := EndDate{parties: parties}
-	r.Compute()
-
-	got := r.task.GetDate()
-
-	want := time.Date(2023, 4, 27, 23, 59, 59, 0, time.UTC)
-
-	if got != want {
-		t.Error(fmt.Print("Got: ", got, "\n", "Want: ", want, "\n"))
-	}
-}
-
-func TestAdvanceNoticeRule(t *testing.T) {
-	parties := []*data.Party{
-		data.Party1,
-		data.Party2,
-	}
-
-	r := AdvanceNoticeDeadline{parties: parties}
-	r.Compute()
-
-	got := r.task.GetDate()
-
-	want := time.Date(2023, 1, 27, 23, 59, 59, 0, time.UTC)
-
-	if got != want {
-		t.Error(fmt.Print("Got: ", got, "\n", "Want: ", want, "\n"))
-	}
-}
-
-func TestPaymentQuantity(t *testing.T) {
-	r := PeriodicPayment{}
-
-	got := r.PeriodicPaymentQuantity()
-	want := 3
-
-	if got != want {
-		t.Error(fmt.Printf("The quantity of payments: %d is differerent to: %d", got, want))
-	}
+	got := rule.GetOutput("contract_signature_date").(time.Time)
+	require.WithinDuration(t, got, time.Now(), time.Second)
 
 }
 
-func TestPeriodicPayment(t *testing.T) {
-	pp := PeriodicPayment{}
-	pp.Compute().Save()
+// func TestStartDateRule(t *testing.T) {
+// 	parties := []*data.Party{
+// 		data.Party1,
+// 		data.Party2,
+// 	}
 
-	got := len(data.ContractInst.GetAttributes())
-	want := 6
+// 	r := StartDate{parties: parties}
+// 	r.Compute()
 
-	if got != want {
-		t.Error(fmt.Printf("The value of payment: %d is differerent to: %d", got, want))
-	}
-}
+// 	got := r.task.GetDate()
 
-func TestTermination(t *testing.T) {
-	r := Termination{}
-	r.Compute().Save()
+// 	want := time.Date(2022, 4, 28, 0, 0, 0, 0, time.UTC)
 
-	attr := data.ContractInst.GetAttributes()
-	got := attr["penalty_payment"].(*data.Payment).Value
-	want := 6.0
+// 	if got != want {
+// 		t.Error(fmt.Print("Got: ", got, "\n", "Want: ", want, "\n"))
+// 	}
+// }
 
-	if got != want {
-		t.Error(fmt.Printf("The value of payment: %f is differerent to: %f", got, want))
-	}
-}
+// func TestEndDateRule(t *testing.T) {
+
+// 	parties := []*data.Party{
+// 		data.Party1,
+// 		data.Party2,
+// 	}
+
+// 	r := EndDate{parties: parties}
+// 	r.Compute()
+
+// 	got := r.task.GetDate()
+
+// 	want := time.Date(2023, 4, 27, 23, 59, 59, 0, time.UTC)
+
+// 	if got != want {
+// 		t.Error(fmt.Print("Got: ", got, "\n", "Want: ", want, "\n"))
+// 	}
+// }
+
+// func TestAdvanceNoticeRule(t *testing.T) {
+// 	parties := []*data.Party{
+// 		data.Party1,
+// 		data.Party2,
+// 	}
+
+// 	r := AdvanceNoticeDeadline{parties: parties}
+// 	r.Compute()
+
+// 	got := r.task.GetDate()
+
+// 	want := time.Date(2023, 1, 27, 23, 59, 59, 0, time.UTC)
+
+// 	if got != want {
+// 		t.Error(fmt.Print("Got: ", got, "\n", "Want: ", want, "\n"))
+// 	}
+// }
+
+// func TestPaymentQuantity(t *testing.T) {
+// 	r := PeriodicPayment{}
+
+// 	got := r.PeriodicPaymentQuantity()
+// 	want := 3
+
+// 	if got != want {
+// 		t.Error(fmt.Printf("The quantity of payments: %d is differerent to: %d", got, want))
+// 	}
+
+// }
+
+// func TestPeriodicPayment(t *testing.T) {
+// 	pp := PeriodicPayment{}
+// 	pp.Compute().Save()
+
+// 	got := len(data.ContractInst.GetAttributes())
+// 	want := 6
+
+// 	if got != want {
+// 		t.Error(fmt.Printf("The value of payment: %d is differerent to: %d", got, want))
+// 	}
+// }
+
+// func TestTermination(t *testing.T) {
+// 	r := Termination{}
+// 	r.Compute().Save()
+
+// 	attr := data.ContractInst.GetAttributes()
+// 	got := attr["penalty_payment"].(*data.Payment).Value
+// 	want := 6.0
+
+// 	if got != want {
+// 		t.Error(fmt.Printf("The value of payment: %f is differerent to: %f", got, want))
+// 	}
+// }
--- rules/task_rules.go
+++ rules/task_rules.go
@@ -7,151 +7,335 @@ import (
 	"github.com/danielmachado86/contracts/data"
 )
 
-func roundDateToNextDay(t time.Time) time.Time {
-	return time.Date(
-		t.Year(),
-		t.Month(),
-		t.Day()+1,
-		0, 0, 0, 0,
-		t.Location(),
-	)
+// func roundDateToNextDay(t time.Time) time.Time {
+// 	return time.Date(
+// 		t.Year(),
+// 		t.Month(),
+// 		t.Day()+1,
+// 		0, 0, 0, 0,
+// 		t.Location(),
+// 	)
+// }
+
+func getParams(attributes *Attributes) map[string]interface{} {
+	params := make(map[string]interface{})
+	for _, param := range attributes.Parameters {
+		params[param.Name] = param.Value
+	}
+	return params
 }
 
-func createTask(n string, d time.Time) *data.Task {
-	t := &data.Task{Name: n, Date: d}
-	return t
-}
-
-// Define las condiciones para calcular la fecha de firma de contrato
-type SignatureDate struct {
-	task    *data.Task
-	parties []*data.Party
-}
-
-// Calcula fecha de firma de contrato
-func (r *SignatureDate) Compute() Rule {
-	t := time.Date(1970, 1, 1, 1, 0, 0, 0, time.UTC)
-	for _, p := range r.parties {
-		d := data.GetSignature(p).Date
-		if d.After(t) {
-			t = d
+func checkInputs(inputs []string, ruleRegistry map[string]Rule) error {
+	var err error
+	for _, input := range inputs {
+		rule := ruleRegistry[input]
+		err = rule.Run(ruleRegistry)
+		if err != nil {
+			return err
 		}
 	}
-	r.task = createTask("signature_date", t)
-	return r
+	return nil
 }
 
-func (r *SignatureDate) Save() {
-	r.task.Save()
-}
+type MaxPartiesFactory struct{}
 
-type StartDate struct {
-	parties []*data.Party
-	task    *data.Task
-}
-
-func (r *StartDate) Compute() Rule {
-
-	params := data.GetParams()
-
-	sr := &SignatureDate{parties: r.parties}
-	o := params.Offset
-
-	sr.Compute()
-	rounded := roundDateToNextDay(sr.task.GetDate())
-	sd := rounded.AddDate(o.Years, o.Months, o.Days)
-	r.task = createTask("start_date", sd)
-	return r
-}
-
-func (r *StartDate) Save() {
-	r.task.Save()
-}
-
-type EndDate struct {
-	parties []*data.Party
-	task    *data.Task
-}
-
-func (r *EndDate) Compute() Rule {
-
-	params := data.GetParams()
-
-	d := params.Duration
-
-	// Start rule
-	sr := &StartDate{
-		parties: r.parties,
+func (factory *MaxPartiesFactory) Create(attributes *Attributes) Rule {
+	params := getParams(attributes)
+	return &Parties{
+		Name:    attributes.Name,
+		Max:     int(params["max"].(float64)),
+		Min:     int(params["min"].(float64)),
+		Inputs:  attributes.Inputs,
+		Outputs: make(map[string]interface{}),
 	}
-	// Termination date
-	sr.Compute()
-	td := sr.task.AddPeriod(d).Add(-time.Second * 1)
-
-	r.task = createTask("end_date", td)
-	return r
 }
 
-func (r *EndDate) Save() {
-	r.task.Save()
+type Parties struct {
+	Name    string
+	Max     int
+	Min     int
+	Parties []*data.Party
+	Inputs  []string
+	Outputs map[string]interface{}
 }
 
-type AdvanceNoticeDeadline struct {
-	parties []*data.Party
-	task    *data.Task
+func NewPartiesRule(attributes *Attributes) (Rule, error) {
+	factory := MaxPartiesFactory{}
+	return factory.Create(attributes), nil
 }
 
-func (r *AdvanceNoticeDeadline) Compute() Rule {
+func (rule *Parties) Run(ruleRegistry map[string]Rule) error {
 
-	params := data.GetParams()
+	err := checkInputs(rule.Inputs, ruleRegistry)
+	if err != nil {
+		return err
+	}
 
-	// Period
-	p := params.PeriodAN
-
-	// End date rule
-	er := &EndDate{parties: r.parties}
-	er.Compute()
-	// End date
-	ed := er.task.GetDate()
-	// Advance notice deadline
-	nd := ed.AddDate(-p.Years, -p.Months, -p.Days)
-
-	r.task = createTask("advance_notice_deadline", nd)
-	return r
+	rule.Outputs[rule.Name] = rule.Parties
+	return nil
 }
 
-func (r *AdvanceNoticeDeadline) Save() {
-	r.task.Save()
+func (rule *Parties) GetName() string {
+	return rule.Name
 }
 
-type NotificationDate struct {
-	parties []*data.Party
-	payment int
-	task    *data.Task
+func (rule *Parties) GetOutput(outputName string) interface{} {
+	return rule.Outputs[outputName]
 }
 
-func (r *NotificationDate) Compute() Rule {
+type SignaturesFactory struct{}
 
-	return r
+func (factory *SignaturesFactory) Create(attributes *Attributes) Rule {
+	return &Signatures{
+		Name:    attributes.Name,
+		Inputs:  attributes.Inputs,
+		Outputs: make(map[string]interface{}),
+	}
 }
 
-func (r *NotificationDate) Save() {
-	r.task.Save()
+type Signatures struct {
+	Name    string
+	Inputs  []string
+	Outputs map[string]interface{}
 }
 
-type PaymentDueDate struct {
-	name string
-	time time.Time
-	task *data.Task
+func NewSignaturesRule(attributes *Attributes) (Rule, error) {
+	factory := SignaturesFactory{}
+	return factory.Create(attributes), nil
 }
 
-func (r *PaymentDueDate) Compute() Rule {
+func (rule *Signatures) Run(ruleRegistry map[string]Rule) error {
 
-	//Payment closing date
-	pd := r.time.AddDate(0, 0, 5)
-	r.task = createTask(fmt.Sprintf("%s_due_date", r.name), pd)
-	return r
+	err := checkInputs(rule.Inputs, ruleRegistry)
+	if err != nil {
+		return err
+	}
+
+	parties := ruleRegistry["contract_parties"].GetOutput("contract_parties").([]*data.Party)
+
+	var signatures []data.Signature
+	for _, party := range parties {
+		signature := data.GetSignature(party)
+		if signature != nil {
+			signatures = append(signatures, *signature)
+		}
+	}
+	rule.Outputs[rule.Name] = signatures
+	return nil
 }
 
-func (r *PaymentDueDate) Save() {
-	r.task.Save()
+func (rule *Signatures) GetName() string {
+	return rule.Name
 }
+
+func (rule *Signatures) GetOutput(outputName string) interface{} {
+	return rule.Outputs[outputName]
+}
+
+type IsSignedFactory struct{}
+
+func (factory *IsSignedFactory) Create(attributes *Attributes) Rule {
+	params := getParams(attributes)
+	return &IsSigned{
+		Name:       attributes.Name,
+		Signatures: int(params["signatures"].(float64)),
+		Inputs:     attributes.Inputs,
+		Outputs:    make(map[string]interface{}),
+	}
+}
+
+type IsSigned struct {
+	Name       string
+	Signatures int
+	Inputs     []string
+	Outputs    map[string]interface{}
+}
+
+func NewIsSignedRule(attributes *Attributes) (Rule, error) {
+	factory := IsSignedFactory{}
+	return factory.Create(attributes), nil
+}
+
+func (rule *IsSigned) Run(ruleRegistry map[string]Rule) error {
+
+	err := checkInputs(rule.Inputs, ruleRegistry)
+	if err != nil {
+		return err
+	}
+
+	signatures := ruleRegistry["contract_signatures"].GetOutput("contract_signatures").([]data.Signature)
+
+	if len(signatures) == rule.Signatures {
+		rule.Outputs[rule.Name] = true
+	} else {
+		rule.Outputs[rule.Name] = false
+	}
+
+	return nil
+}
+
+func (rule *IsSigned) GetName() string {
+	return rule.Name
+}
+
+func (rule *IsSigned) GetOutput(outputName string) interface{} {
+	return rule.Outputs[outputName]
+}
+
+type SignatureDateFactory struct{}
+
+func (factory *SignatureDateFactory) Create(attributes *Attributes) Rule {
+	return &SignatureDate{
+		Name:    attributes.Name,
+		Inputs:  attributes.Inputs,
+		Outputs: make(map[string]interface{}),
+	}
+}
+
+type SignatureDate struct {
+	Name    string
+	Inputs  []string
+	Outputs map[string]interface{}
+}
+
+func NewSignatureDateRule(attributes *Attributes) (Rule, error) {
+	factory := SignatureDateFactory{}
+	return factory.Create(attributes), nil
+}
+
+func (rule *SignatureDate) Run(ruleRegistry map[string]Rule) error {
+
+	err := checkInputs(rule.Inputs, ruleRegistry)
+	if err != nil {
+		return err
+	}
+
+	is_signed := ruleRegistry["contract_is_signed"].GetOutput("contract_is_signed").(bool)
+	signatures := ruleRegistry["contract_signatures"].GetOutput("contract_signatures").([]data.Signature)
+
+	if !is_signed {
+		return fmt.Errorf("contract not signed")
+	}
+
+	ld := time.Date(1970, 1, 1, 1, 0, 0, 0, time.UTC)
+	for _, s := range signatures {
+		d := s.Date
+		if d.After(ld) {
+			ld = d
+		}
+	}
+	rule.Outputs[rule.Name] = ld
+	return nil
+}
+
+func (rule *SignatureDate) GetName() string {
+	return rule.Name
+}
+
+func (rule *SignatureDate) GetOutput(outputName string) interface{} {
+	return rule.Outputs[outputName]
+}
+
+// type StartDate struct {
+// }
+
+// func (r *StartDate) Run(inputs []Field, contract data.Contract) {
+
+// 	exist := fieldExist(contract, CONTRACT_START_DATE)
+// 	if exist {
+// 		return
+// 	}
+
+// 	checkInputs(inputs, contract)
+
+// 	sg := contract.Terms[CONTRACT_SIGNATURE_DATE].(time.Time)
+// 	rounded := roundDateToNextDay(sg)
+// 	contract.Terms[CONTRACT_SIGNATURE_DATE] = rounded
+// 	return
+// }
+
+// type EndDate struct {
+// }
+
+// func (r *EndDate) Run(inputs []Field, contract data.Contract) {
+
+// 	exist := fieldExist(contract, CONTRACT_END_DATE)
+// 	if exist {
+// 		return
+// 	}
+
+// 	checkInputs(inputs, contract)
+
+// 	sd := contract.Terms[CONTRACT_START_DATE].(time.Time)
+
+// 	td := sd.AddDate().Add(-time.Second * 1)
+
+// 	contract.Terms[END_DATE_RULE_NAME] = td
+
+// 	return
+// }
+
+// func (r *EndDate) Save() {
+// 	r.task.Save()
+// }
+
+// type AdvanceNoticeDeadline struct {
+// 	parties []*data.Party
+// 	task    *data.Task
+// }
+
+// func (r *AdvanceNoticeDeadline) Compute() Rule {
+
+// 	params := data.GetParams()
+
+// 	// Period
+// 	p := params.PeriodAN
+
+// 	// End date rule
+// 	er := &EndDate{parties: r.parties}
+// 	er.Compute()
+// 	// End date
+// 	ed := er.task.GetDate()
+// 	// Advance notice deadline
+// 	nd := ed.AddDate(-p.Years, -p.Months, -p.Days)
+
+// 	r.task = createTask("advance_notice_deadline", nd)
+// 	return r
+// }
+
+// func (r *AdvanceNoticeDeadline) Save() {
+// 	r.task.Save()
+// }
+
+// type NotificationDate struct {
+// 	parties []*data.Party
+// 	payment int
+// 	task    *data.Task
+// }
+
+// func (r *NotificationDate) Compute() Rule {
+
+// 	return r
+// }
+
+// func (r *NotificationDate) Save() {
+// 	r.task.Save()
+// }
+
+// type PaymentDueDate struct {
+// 	name string
+// 	time time.Time
+// 	task *data.Task
+// }
+
+// func (r *PaymentDueDate) Compute() Rule {
+
+// 	//Payment closing date
+// 	pd := r.time.AddDate(0, 0, 5)
+// 	r.task = createTask(fmt.Sprintf("%s_due_date", r.name), pd)
+// 	return r
+// }
+
+// func (r *PaymentDueDate) Save() {
+// 	r.task.Save()
+// }
