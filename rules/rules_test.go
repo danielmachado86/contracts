021e12a4dde1edf4b7e274f1dfb2518cef4b553a
Daniel Machado <danielmcis@gmail.com>
2022-07-27 16:52:04 +0000

    Rules engine tests added

--- rules/rules_test.go
+++ rules/rules_test.go
@@ -6,29 +6,34 @@ import (
 	"testing"
 	"time"
 
-	"github.com/danielmachado86/contracts/data"
+	db "github.com/danielmachado86/contracts/db/sqlc"
 	"github.com/stretchr/testify/require"
 )
 
-func TestSignatureDateRule(t *testing.T) {
-
-	file, err := ioutil.ReadFile("rental_contract.json")
+func loadTemplateFile(t *testing.T, template_file string) (TemplateSpecification, error) {
+	file, err := ioutil.ReadFile(template_file)
 	require.NoError(t, err)
 
 	template := TemplateSpecification{}
 	err = json.Unmarshal([]byte(file), &template)
+	return template, err
+}
+
+func TestParties(t *testing.T) {
+
+	template, err := loadTemplateFile(t, "rental_contract.json")
 	require.NoError(t, err)
 
-	user1 := data.NewUser("Daniel M")
-	user2 := data.NewUser("Jimena L")
+	party1 := &db.Party{
+		Username:   "DanielM",
+		ContractID: 1,
+	}
+	party2 := &db.Party{
+		Username:   "JimenaL",
+		ContractID: 1,
+	}
 
-	party1 := data.NewParty(user1)
-	party2 := data.NewParty(user2)
-
-	data.NewSignature(party1)
-	data.NewSignature(party2)
-
-	var parties []*data.Party
+	var parties []*db.Party
 	parties = append(parties, party1)
 	parties = append(parties, party2)
 
@@ -41,6 +46,162 @@ func TestSignatureDateRule(t *testing.T) {
 
 	ruleRegistry["contract_parties"].(*Parties).Parties = parties
 
+	rule := ruleRegistry["contract_parties"]
+	err = rule.Run(ruleRegistry)
+	require.NoError(t, err)
+
+	got := rule.GetOutput("contract_parties").([]*db.Party)
+	require.Len(t, got, 2)
+}
+
+func TestSignatures(t *testing.T) {
+
+	template, err := loadTemplateFile(t, "rental_contract.json")
+	require.NoError(t, err)
+
+	party1 := &db.Party{
+		Username:   "DanielM",
+		ContractID: 1,
+	}
+	party2 := &db.Party{
+		Username:   "JimenaL",
+		ContractID: 1,
+	}
+
+	var parties []*db.Party
+	parties = append(parties, party1)
+	parties = append(parties, party2)
+
+	var signatureList = []*db.Signature{}
+
+	signature1 := &db.Signature{
+		Username:   party1.Username,
+		ContractID: party1.ContractID,
+		CreatedAt:  time.Now(),
+	}
+	signature2 := &db.Signature{
+		Username:   party2.Username,
+		ContractID: party2.ContractID,
+		CreatedAt:  time.Now(),
+	}
+
+	signatureList = append(signatureList, signature1)
+	signatureList = append(signatureList, signature2)
+
+	ruleRegistry := make(map[string]Rule)
+
+	for _, attribute := range template.Attributes {
+		ruleRegistry[attribute.Name], err = CreateRule(attribute)
+		require.NoError(t, err)
+	}
+
+	ruleRegistry["contract_parties"].(*Parties).Parties = parties
+	ruleRegistry["contract_signatures"].(*Signatures).Signatures = signatureList
+
+	rule := ruleRegistry["contract_signatures"]
+	err = rule.Run(ruleRegistry)
+	require.NoError(t, err)
+
+	got := rule.GetOutput("contract_signatures").([]*db.Signature)
+	require.Len(t, got, 2)
+}
+
+func TestIsSignedRule(t *testing.T) {
+
+	template, err := loadTemplateFile(t, "rental_contract.json")
+	require.NoError(t, err)
+
+	party1 := &db.Party{
+		Username:   "DanielM",
+		ContractID: 1,
+	}
+	party2 := &db.Party{
+		Username:   "JimenaL",
+		ContractID: 1,
+	}
+
+	var parties []*db.Party
+	parties = append(parties, party1)
+	parties = append(parties, party2)
+
+	var signatureList = []*db.Signature{}
+
+	signature1 := &db.Signature{
+		Username:   party1.Username,
+		ContractID: party1.ContractID,
+		CreatedAt:  time.Now(),
+	}
+	signature2 := &db.Signature{
+		Username:   party2.Username,
+		ContractID: party2.ContractID,
+		CreatedAt:  time.Now(),
+	}
+
+	signatureList = append(signatureList, signature1)
+	signatureList = append(signatureList, signature2)
+
+	ruleRegistry := make(map[string]Rule)
+
+	for _, attribute := range template.Attributes {
+		ruleRegistry[attribute.Name], err = CreateRule(attribute)
+		require.NoError(t, err)
+	}
+
+	ruleRegistry["contract_parties"].(*Parties).Parties = parties
+	ruleRegistry["contract_signatures"].(*Signatures).Signatures = signatureList
+
+	rule := ruleRegistry["contract_is_signed"]
+	err = rule.Run(ruleRegistry)
+	require.NoError(t, err)
+
+	got := rule.GetOutput("contract_is_signed").(bool)
+	require.True(t, got)
+}
+
+func TestSignatureDateRule(t *testing.T) {
+
+	template, err := loadTemplateFile(t, "rental_contract.json")
+	require.NoError(t, err)
+
+	party1 := &db.Party{
+		Username:   "DanielM",
+		ContractID: 1,
+	}
+	party2 := &db.Party{
+		Username:   "JimenaL",
+		ContractID: 1,
+	}
+
+	var parties []*db.Party
+	parties = append(parties, party1)
+	parties = append(parties, party2)
+
+	var signatureList = []*db.Signature{}
+
+	signature1 := &db.Signature{
+		Username:   party1.Username,
+		ContractID: party1.ContractID,
+		CreatedAt:  time.Now(),
+	}
+	signature2 := &db.Signature{
+		Username:   party2.Username,
+		ContractID: party2.ContractID,
+		CreatedAt:  time.Now(),
+	}
+
+	signatureList = append(signatureList, signature1)
+	signatureList = append(signatureList, signature2)
+
+	ruleRegistry := make(map[string]Rule)
+
+	for _, attribute := range template.Attributes {
+		ruleRegistry[attribute.Name], err = CreateRule(attribute)
+		require.NoError(t, err)
+	}
+
+	ruleRegistry["contract_parties"].(*Parties).Parties = parties
+	ruleRegistry["contract_signatures"].(*Signatures).Signatures = signatureList
+
 	rule := ruleRegistry["contract_signature_date"]
 	err = rule.Run(ruleRegistry)
 	require.NoError(t, err)
@@ -50,94 +211,69 @@ func TestSignatureDateRule(t *testing.T) {
 
 }
 
-// func TestStartDateRule(t *testing.T) {
-// 	parties := []*data.Party{
-// 		data.Party1,
-// 		data.Party2,
-// 	}
+func TestSAllRules(t *testing.T) {
 
-// 	r := StartDate{parties: parties}
-// 	r.Compute()
+	template, err := loadTemplateFile(t, "rental_contract.json")
+	require.NoError(t, err)
 
-// 	got := r.task.GetDate()
+	party1 := &db.Party{
+		Username:   "DanielM",
+		ContractID: 1,
+	}
+	party2 := &db.Party{
+		Username:   "JimenaL",
+		ContractID: 1,
+	}
 
-// 	want := time.Date(2022, 4, 28, 0, 0, 0, 0, time.UTC)
+	var partyList []*db.Party
+	partyList = append(partyList, party1)
+	partyList = append(partyList, party2)
 
-// 	if got != want {
-// 		t.Error(fmt.Print("Got: ", got, "\n", "Want: ", want, "\n"))
-// 	}
-// }
+	var signatureList []*db.Signature
 
-// func TestEndDateRule(t *testing.T) {
+	signature1 := &db.Signature{
+		Username:   party1.Username,
+		ContractID: party1.ContractID,
+		CreatedAt:  time.Now(),
+	}
+	signature2 := &db.Signature{
+		Username:   party2.Username,
+		ContractID: party2.ContractID,
+		CreatedAt:  time.Now(),
+	}
 
-// 	parties := []*data.Party{
-// 		data.Party1,
-// 		data.Party2,
-// 	}
+	signatureList = append(signatureList, signature1)
+	signatureList = append(signatureList, signature2)
 
-// 	r := EndDate{parties: parties}
-// 	r.Compute()
+	ruleRegistry := make(map[string]Rule)
 
-// 	got := r.task.GetDate()
+	for _, attribute := range template.Attributes {
+		ruleRegistry[attribute.Name], err = CreateRule(attribute)
+		require.NoError(t, err)
+	}
 
-// 	want := time.Date(2023, 4, 27, 23, 59, 59, 0, time.UTC)
+	ruleRegistry["contract_parties"].(*Parties).Parties = partyList
+	ruleRegistry["contract_signatures"].(*Signatures).Signatures = signatureList
 
-// 	if got != want {
-// 		t.Error(fmt.Print("Got: ", got, "\n", "Want: ", want, "\n"))
-// 	}
-// }
+	for _, rule := range ruleRegistry {
+		err = rule.Run(ruleRegistry)
+		require.NoError(t, err)
+	}
 
-// func TestAdvanceNoticeRule(t *testing.T) {
-// 	parties := []*data.Party{
-// 		data.Party1,
-// 		data.Party2,
-// 	}
+	rule := ruleRegistry["contract_parties"]
+	parties := rule.GetOutput("contract_parties").([]*db.Party)
+	require.Len(t, parties, 2)
 
-// 	r := AdvanceNoticeDeadline{parties: parties}
-// 	r.Compute()
+	rule = ruleRegistry["contract_signatures"]
+	signatures := rule.GetOutput("contract_signatures").([]*db.Signature)
+	require.Len(t, signatures, 2)
 
-// 	got := r.task.GetDate()
+	rule = ruleRegistry["contract_is_signed"]
+	is_signed := rule.GetOutput("contract_is_signed").(bool)
+	require.True(t, is_signed)
 
-// 	want := time.Date(2023, 1, 27, 23, 59, 59, 0, time.UTC)
+	rule = ruleRegistry["contract_signature_date"]
+	signature_date := rule.GetOutput("contract_signature_date").(time.Time)
+	require.WithinDuration(t, signature_date, time.Now(), time.Second)
 
-// 	if got != want {
-// 		t.Error(fmt.Print("Got: ", got, "\n", "Want: ", want, "\n"))
-// 	}
-// }
-
-// func TestPaymentQuantity(t *testing.T) {
-// 	r := PeriodicPayment{}
-
-// 	got := r.PeriodicPaymentQuantity()
-// 	want := 3
-
-// 	if got != want {
-// 		t.Error(fmt.Printf("The quantity of payments: %d is differerent to: %d", got, want))
-// 	}
-
-// }
-
-// func TestPeriodicPayment(t *testing.T) {
-// 	pp := PeriodicPayment{}
-// 	pp.Compute().Save()
-
-// 	got := len(data.ContractInst.GetAttributes())
-// 	want := 6
-
-// 	if got != want {
-// 		t.Error(fmt.Printf("The value of payment: %d is differerent to: %d", got, want))
-// 	}
-// }
-
-// func TestTermination(t *testing.T) {
-// 	r := Termination{}
-// 	r.Compute().Save()
-
-// 	attr := data.ContractInst.GetAttributes()
-// 	got := attr["penalty_payment"].(*data.Payment).Value
-// 	want := 6.0
-
-// 	if got != want {
-// 		t.Error(fmt.Printf("The value of payment: %f is differerent to: %f", got, want))
-// 	}
-// }
+}
